3ed17a948a0d12aedffcc6dbd7e1e5d9e7091d15
Megan McClure <meganemcclure@gmail.com>
2021-07-05 14:16:36 -0400

    adjust spacing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* globals RavelryApi */
 
-RavelryApi = function(base, authUsername, authPassword) {
+RavelryApi = function (base, authUsername, authPassword) {
   this.base = base;
   this.authUsername = authUsername;
   this.authPassword = authPassword;
@@ -8,26 +8,26 @@ RavelryApi = function(base, authUsername, authPassword) {
 };
 
 
-RavelryApi.prototype.get = function(url) {
+RavelryApi.prototype.get = function (url) {
   const headers = new Headers();
   const debugFunction = this.debugFunction;
   // This is the HTTP header that you need add in order to access api.ravelry.com with a read only API key
   // `btoa` will base 64 encode a string: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
-  
+
   headers.append('Authorization', 'Basic ' + btoa(this.authUsername + ":" + this.authPassword));
-  
-  return fetch(url, { method: 'GET', headers: headers }).then(function(response) {
+
+  return fetch(url, { method: 'GET', headers: headers }).then(function (response) {
     return response.json();
-  }).then(function(json) { 
+  }).then(function (json) {
     if (debugFunction) debugFunction(json);
-    return json; 
+    return json;
   });
 };
 
 // Retrieve a list of projects for a user: https://www.ravelry.com/api#projects_list
 // Pagination is optional, default is no pagination
 
-RavelryApi.prototype.projectsList = function(username, page) {
+RavelryApi.prototype.projectsList = function (username, page) {
   const pageSize = 25;
   const url = this.base + '/projects/' + username + '/list.json?page=' + page + '&page_size=' + pageSize;
   return this.get(url);
@@ -37,71 +37,58 @@ RavelryApi.prototype.projectsList = function(username, page) {
 
 /* globals ApiDemo */
 
-ApiDemo = function() {
+ApiDemo = function () {
   this.ravelryApiClient = null;
   this.currentProjectPage = null;
-  
-  
+
+
   this.addEventListeners();
 };
 
-ApiDemo.prototype.createApiClient = function(authUsername, authPassword) {
+ApiDemo.prototype.createApiClient = function (authUsername, authPassword) {
   this.ravelryApiClient = new RavelryApi('https://api.ravelry.com', authUsername, authPassword);
-//   this.ravelryApiClient.debugFunction = function(json) {
-//     var inspector = document.getElementById('json-inspector');
-//     inspector.style.display = 'block';
-//     inspector.value = JSON.stringify(json, null, 2);
-//     console.log(json);
-//   };
+  //   this.ravelryApiClient.debugFunction = function(json) {
+  //     var inspector = document.getElementById('json-inspector');
+  //     inspector.style.display = 'block';
+  //     inspector.value = JSON.stringify(json, null, 2);
+  //     console.log(json);
+  //   };
 };
 
 
-ApiDemo.prototype.addEventListeners = function() {
+ApiDemo.prototype.addEventListeners = function () {
   const projectListForm = document.getElementById('projects-list-form');
 
-    const submitProjectSearch = function() {
+  const submitProjectSearch = function () {
     const username = projectListForm.querySelector("input[name='username']").value;
     this.renderProjects(username, this.currentProjectPage);
   }.bind(this);
 
 
   // create an API client when the page is loaded
-    const usernameKey = "read-84bca6d13528b8b3e477d9a019e00417";
-    const passwordKey = "ojGJolAV/FA3qMQs74mZlL0TvVqAG27uERZ607VL";
+  const usernameKey = "read-84bca6d13528b8b3e477d9a019e00417";
+  const passwordKey = "ojGJolAV/FA3qMQs74mZlL0TvVqAG27uERZ607VL";
 
-    this.createApiClient(usernameKey, passwordKey);
+  this.createApiClient(usernameKey, passwordKey);
 
-    document.getElementById('api-request').style.display = 'block';
-    this.currentProjectPage = 1;
-    submitProjectSearch();
+  document.getElementById('api-request').style.display = 'block';
+  this.currentProjectPage = 1;
+  submitProjectSearch();
 
-    
-  projectListForm.onsubmit = function() {
+
+  projectListForm.onsubmit = function () {
     this.currentProjectPage = 1;
     submitProjectSearch();
     return false;
   }.bind(this);
 
-  // const previousLink = document.getElementById('pagination-previous');
-  // const nextLink = document.getElementById('pagination-next');
-
-  // nextLink.addEventListener('click', function() {
-  //   this.currentProjectPage += 1;
-  //   submitProjectSearch();
-  // }.bind(this));
-
-  // previousLink.addEventListener('click', function() {
-  //   this.currentProjectPage -= 1;
-  //   submitProjectSearch();
-  // }.bind(this));
-
 };
 
 
-ApiDemo.prototype.renderProjects = function(username, page) {
+ApiDemo.prototype.renderProjects = function (username, page) {
   document.getElementById('loading_indicator').style.display = 'inline-block';
 
-  this.ravelryApiClient.projectsList(username, page).then(function(json) {
+  this.ravelryApiClient.projectsList(username, page).then(function (json) {
     document.getElementById('loading_indicator').style.display = 'none';
 
     const rootElement = document.getElementById('projects-list-results');
@@ -114,11 +101,11 @@ ApiDemo.prototype.renderProjects = function(username, page) {
     // const nextPageLink = document.getElementById('pagination-next');
     // nextPageLink.style.display = json.paginator.page < json.paginator.last_page ? 'block' : 'none';
 
-    json.projects.forEach(function(project) {
+    json.projects.forEach(function (project) {
       const child = document.createElement('div');
       child.className = 'project__result';
 
-      const img = project.first_photo ? project.first_photo.square_url : "";
+      const img = project.first_photo ? project.first_photo.square_url : "./knit-icon.jpg";
       child.style.backgroundImage = 'url(' + img + ')';
 
       const title = document.createElement('a');
@@ -128,19 +115,16 @@ ApiDemo.prototype.renderProjects = function(username, page) {
       title.href = project.links.self.href;
       titleText.innerText = project.name;
       title.appendChild(titleText);
-      // titleText.innerHTML = project.name;
-      // title.innerText = project.name;
-      // title.appendChild(innerText);
       child.appendChild(title);
 
       rootElement.appendChild(child);
     });
-  });  
+  });
 };
 
 // render content when ready
 if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function () {
     new ApiDemo();
   });
 } else {
